973f0fbadfd5649c996c9e5401968bb27f5204de
StoneColdEduardo <156251883+StoneColdEduardo@users.noreply.github.com>
2024-01-23 15:30:36 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -32,25 +32,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 function damageCounter (bool: boolean, mySprite: Sprite) {
-    game.gameOver(true)
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+	
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
     info.changeLifeBy(-1)
@@ -413,7 +395,6 @@ controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Press
         )
     }
 })
-let mySprite: Sprite = null
 let projectile1: Sprite = null
 let jump = 0
 let player2: Sprite = null
